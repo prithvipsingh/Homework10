7dffceab915516fb201bba7aaa19a2d4d0397b0e
prithvipsingh <ppsdeora@gmail.com>
2020-04-30 18:57:18 +1000

    "add css file"

--- Develop/app.js
+++ Develop/app.js
@@ -9,10 +9,10 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
- // create employee array which hold all employess
+// create employee  object array which hold all employess
 
 const employees = [];
-// array of object employee questions
+// create  array of object employee questions
 
 const employeeQuestions = [
   {
@@ -29,97 +29,97 @@ const employeeQuestions = [
     type: "input",
     name: "email",
     message: "What is the email of employee ?",
-  }
-]
+  },
+];
 // object for Manager Question
 const managerQuestion = {
-    type: "number",
-    name: "officeNumber",
-    message: "What is office number ?"
-}
+  type: "number",
+  name: "officeNumber",
+  message: "What is office number ?",
+};
 // object for employee  Question
 const employeeTypeQuestion = {
-    type: "list",
-    name: "option",
-    message: "What do you want to do ?",
-    choices: [
-        "Add a new Engineer",
-        "Add a new Intern",
-        "Exit"
-    ]
-}
- // object for engineer question 
+  type: "list",
+  name: "option",
+  message: "What do you want to do ?",
+  choices: ["Add a new Engineer", "Add a new Intern", "Exit"],
+};
+// object for engineer question
 const engineerQuestion = {
-    type: "input",
-    name:  "github",
-    message: "What is your gitHub profile ?"
-}
-// object for intern question 
+  type: "input",
+  name: "github",
+  message: "What is your gitHub profile ?",
+};
+// object for intern question
 const internQuestion = {
-    type: "input",
-    name:  "school",
-    message: "What is name of school ?"
-}
-// creating a function for manager data 
+  type: "input",
+  name: "school",
+  message: "What is name of school ?",
+};
+// creating a function for manager data
 function init() {
-    inquirer 
-        .prompt([...employeeQuestions, managerQuestion])
-        .then((answers)=>{
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-            employees.push(manager);
-            createEmployees();
-        })
+  inquirer.prompt([...employeeQuestions, managerQuestion]).then((answers) => {
+    const manager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.officeNumber
+    );
+    employees.push(manager);
+    createEmployees();
+  });
 }
 
-function createEmployees(){
-    inquirer 
-        .prompt(employeeTypeQuestion)
-        .then((answer)=>{
-            //switch 
-            switch (answer.option){
-                case "Add a new Engineer":
-                    createEngineer();
-                    break;
-                case "Add a new Intern":
-                    createIntern();
-                    break;
-                default:
-                    generateHtml(employees);  
-            }      
-        })
-}
-
-function generateHtml(employees){
-    const html = render(employees);
-
-    if (!fs.existsSync(OUTPUT_DIR)){
-        fs.mkdirSync(OUTPUT_DIR);
+function createEmployees() {
+  inquirer.prompt(employeeTypeQuestion).then((answer) => {
+    switch (answer.option) {
+      case "Add a new Engineer":
+        createEngineer();
+        break;
+      case "Add a new Intern":
+        createIntern();
+        break;
+      default:
+        generateHtml(employees);
     }
+  });
+}
 
-    fs.writeFileSync(outputPath, html, "utf-8");
+function generateHtml(employees) {
+  const html = render(employees);
+
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+
+  fs.writeFileSync(outputPath, html, "utf-8");
 }
 
 function createEngineer() {
-    inquirer 
-        .prompt([...employeeQuestions, engineerQuestion])
-        .then((answers)=>{
-            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-            employees.push(engineer);
-            createEmployees();
-        })
+  inquirer.prompt([...employeeQuestions, engineerQuestion]).then((answers) => {
+    const engineer = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
+    );
+    employees.push(engineer);
+    createEmployees();
+  });
 }
-
 
 function createIntern() {
-    inquirer 
-        .prompt([...employeeQuestions, internQuestion])
-        .then((answers)=>{
-            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-            employees.push(intern);
-            createEmployees();
-        })
+  inquirer.prompt([...employeeQuestions, internQuestion]).then((answers) => {
+    const intern = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.school
+    );
+    employees.push(intern);
+    createEmployees();
+  });
 }
-
 
 init();
 
